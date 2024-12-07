2bfb4a27003d2a7161df0a02f8d2e00f0bd3becd
Muhammad-Moeed <m.moeedq497@gmail.com>
2024-12-07 16:34:30 +0500

    in progress

---
+++ app.js
@@ -0,0 +1,115 @@
+let savedEmail = '';
+let savedPassword = '';
+let savedUsername = '';
+
+// Sign up 
+
+function signup() {
+  savedUsername = document.getElementById('signupUsername').value;
+  savedEmail = document.getElementById('signupEmail').value;
+  savedPassword = document.getElementById('signupPassword').value;
+
+  swal({
+    title: "Signup Successful!",
+    text: `Welcome, ${savedUsername}! Your account has been created.`,
+    icon: "success",
+    button: "OK",
+  });
+  console.log('User Information:', {
+    username: savedUsername,
+    email: savedEmail,
+    password: savedPassword
+  });
+
+  return false; // Prevent form submission and page reload
+}
+
+// Login 
+
+function login() {
+    const loginEmail = document.getElementById('loginEmail').value;
+    const loginPassword = document.getElementById('loginPassword').value;
+  
+    if (loginEmail !== savedEmail) {
+      swal("Error!", "Invalid email!", "error");
+    } else if (loginPassword !== savedPassword) {
+      swal("Error!", "Invalid password!", "error");
+    } else {
+        swal({
+            title: "Success!",
+            text: "Login successful!",
+            icon: "success",
+            button: "OK",
+          }).then(() => {
+            // Afetr Sign in redirect welcome page
+            localStorage.setItem('username', savedUsername);
+            window.location.href = "./routes/welcome.html";
+          });
+      
+          console.log("Logged in User:", {
+            username: savedUsername,
+            email: savedEmail,
+          });
+      console.log("Logged in User:", {
+        username: savedUsername,
+        email: savedEmail,
+      });
+    }
+  
+  
+  return false; // Prevent form submission and page reload
+}
+
+// Logout 
+
+function logout() {
+    savedEmail = '';
+    savedPassword = '';
+    savedUsername = '';
+  
+    swal({
+      title: "Logged Out",
+      text: "You have been successfully logged out.",
+      icon: "info",
+      button: "OK",
+    }).then(() => {
+      window.location.href = "../index.html";
+    });
+  
+    console.log("User logged out");
+  }
+
+  const username = localStorage.getItem('username');
+document.getElementById('name').innerHTML = `
+${username}
+`
+const username1 = localStorage.getItem('username');
+document.getElementById('myname').innerHTML = `
+Welcome , ${username}
+`
+
+// Redirect pages 
+
+function webdev(){
+    window.location.href = "./webdev/index.html";
+}
+function python(){
+  window.location.href = "./python/index.html";
+}
+function module(){
+  window.location.href = "./module/index.html";
+}
+
+// Redirect Quiz Window
+function htmlcss(){
+  window.location.href = "./htmlcss.html";
+}
+
+function js(){
+  window.location.href = "./js.html";
+}
+
+
+
+
+
---
+++ quiz.js
@@ -0,0 +1,436 @@
+var htmlCssQuiz = [
+    {
+        que: "What does HTML stand for?",
+        ans1: "Hyper Text Markup Language",
+        ans2: "High Text Machine Language",
+        ans3: "Hyperlink and Text Markup Language",
+        ans4: "Home Tool Markup Language",
+        anskey: "Hyper Text Markup Language"
+    },
+    {
+        que: "Which HTML tag is used to define an internal style sheet?",
+        ans1: "&lt;script&gt;",
+        ans2: "&lt;style&gt;",
+        ans3: "&lt;css&gt;",
+        ans4: "&lt;link&gt;",
+        anskey: "&lt;style&gt;"
+    },
+    {
+        que: "What is the correct HTML element for the largest heading?",
+        ans1: "&lt;h1&gt",
+        ans2: "&lt;heading&gt",
+        ans3: "&lt;h6&gt",
+        ans4: "&lt;head&gt",
+        anskey: "&lt;h1&gt"
+    },
+    {
+        que: "Which property is used in CSS to change the background color?",
+        ans1: "background-color",
+        ans2: "bgcolor",
+        ans3: "color",
+        ans4: "background",
+        anskey: "background-color"
+    },
+    {
+        que: "Which is the correct syntax for adding a comment in CSS?",
+        ans1: "// This is a comment",
+        ans2: "/* This is a comment */",
+        ans3: "&lt;!-- This is a comment --&gt;",
+        ans4: "' This is a comment",
+        anskey: "/* This is a comment */"
+    },
+    {
+        que: "Which HTML attribute specifies an alternate text for an image if the image cannot be displayed?",
+        ans1: "alt",
+        ans2: "src",
+        ans3: "title",
+        ans4: "href",
+        anskey: "alt"
+    },
+    {
+        que: "Which CSS property is used to change the text color of an element?",
+        ans1: "text-color",
+        ans2: "color",
+        ans3: "font-color",
+        ans4: "background-color",
+        anskey: "color"
+    },
+    {
+        que: "How do you make a list that lists the items with bullets in HTML?",
+        ans1: "&lt;list&gt",
+        ans2: "&lt;ol&gt",
+        ans3: "&lt;ul&gt",
+        ans4: "&lt;dl&gt",
+        anskey: "&lt;ul&gt"
+    },
+    {
+        que: "Which CSS property controls the text size?",
+        ans1: "font-style",
+        ans2: "text-style",
+        ans3: "font-size",
+        ans4: "text-size",
+        anskey: "font-size"
+    },
+    {
+        que: "How can you open a link in a new tab or window in HTML?",
+        ans1: "target='_self'",
+        ans2: "target='_blank'",
+        ans3: "target='_new'",
+        ans4: "target='_parent'",
+        anskey: "target='_blank'"
+    },
+    {
+        que: "Which HTML tag is used to create a table?",
+        ans1: "&lt;table&gt",
+        ans2: "&lt;tr&gt",
+        ans3: "&lt;td&gt",
+        ans4: "&lt;tab&gt",
+        anskey: "&lt;table&gt"
+    },
+    {
+        que: "Which HTML attribute is used to define inline styles?",
+        ans1: "class",
+        ans2: "style",
+        ans3: "id",
+        ans4: "styles",
+        anskey: "style"
+    },
+    {
+        que: "What is the correct CSS syntax to make all paragraphs bold?",
+        ans1: "p {font-weight: bold;}",
+        ans2: "&lt;p style='font-weight: bold';&gt",
+        ans3: "p {text-size: bold;}",
+        ans4: "&lt;p style='text-size: bold;'&gt",
+        anskey: "p {font-weight: bold;}"
+    },
+    {
+        que: "Which HTML tag is used to define a hyperlink?",
+        ans1: "&lt;a&gt",
+        ans2: "&lt;link&gt",
+        ans3: "&lt;href&gt",
+        ans4: "&lt;hyperlink&gt",
+        anskey: "&lt;a&gt"
+    },
+    {
+        que: "How do you apply a class in HTML?",
+        ans1: "class='classname'",
+        ans2: "style='classname'",
+        ans3: "id='classname'",
+        ans4: "apply='classname'",
+        anskey: "class='classname'"
+    }
+];
+
+// JavaScript Quiz 
+
+const jsQuiz = [
+    {
+      que: "Which type of JavaScript language is?",
+      ans1: "Object-Oriented",
+      ans2: "Object-Based",
+      ans3: "Assembly-language",
+      ans4: "High-level",
+      anskey: "Object-Based",
+    },
+    {
+      que: "Which of the following is not JavaScript Data Types?",
+      ans1: "Undefined",
+      ans2: "Number",
+      ans3: "Boolean",
+      ans4: "Float",
+      anskey: "Float",
+    },
+    {
+      que: "Which company developed JavaScript?",
+      ans1: "Netscape",
+      ans2: "Bell Labs",
+      ans3: "Sun Microsystems",
+      ans4: "IBM",
+      anskey: "Netscape",
+    },
+    {
+      que: "Inside which HTML element do we put the JavaScript?",
+      ans1: "<script>",
+      ans2: "<js>",
+      ans3: "<scripting>",
+      ans4: "<javascript>",
+      anskey: "<script>",
+    },
+    {
+      que: "Which symbol is used for comments in JavaScript?",
+      ans1: "//",
+      ans2: "/* */",
+      ans3: "#",
+      ans4: "<!-- -->",
+      anskey: "//",
+    },
+    {
+      que: "How can you convert a string to an integer in JavaScript?",
+      ans1: "parseInt()",
+      ans2: "Integer()",
+      ans3: "int()",
+      ans4: "parse()",
+      anskey: "parseInt()",
+    },
+    {
+      que: "What will `typeof NaN` return?",
+      ans1: "Object",
+      ans2: "Number",
+      ans3: "String",
+      ans4: "Undefined",
+      anskey: "Number",
+    },
+    {
+      que: "Which JavaScript method is used to write into an alert box?",
+      ans1: "window.alert()",
+      ans2: "document.alert()",
+      ans3: "alertBox()",
+      ans4: "msgAlert()",
+      anskey: "window.alert()",
+    },
+    {
+      que: "Which method is used to remove the last element from an array?",
+      ans1: "shift()",
+      ans2: "pop()",
+      ans3: "delete()",
+      ans4: "splice()",
+      anskey: "pop()",
+    },
+    {
+      que: "What is the output of `Boolean('')` in JavaScript?",
+      ans1: "true",
+      ans2: "false",
+      ans3: "undefined",
+      ans4: "null",
+      anskey: "false",
+    },
+    {
+      que: "What is the purpose of `isNaN` function in JavaScript?",
+      ans1: "Checks if a value is a number",
+      ans2: "Checks if a value is not a number",
+      ans3: "Converts a value to a number",
+      ans4: "Rounds a number",
+      anskey: "Checks if a value is not a number",
+    },
+    {
+      que: "Which operator is used to assign a value to a variable?",
+      ans1: "=",
+      ans2: "==",
+      ans3: "===",
+      ans4: ":",
+      anskey: "=",
+    },
+    {
+      que: "Which JavaScript method can be used to find the length of a string?",
+      ans1: "size()",
+      ans2: "length()",
+      ans3: "strlen()",
+      ans4: ".length",
+      anskey: ".length",
+    },
+    {
+      que: "Which method is used to add new elements to the end of an array?",
+      ans1: "push()",
+      ans2: "add()",
+      ans3: "append()",
+      ans4: "insert()",
+      anskey: "push()",
+    },
+    {
+      que: "What is the purpose of `JSON.stringify` in JavaScript?",
+      ans1: "Converts JSON to a string",
+      ans2: "Parses a string to JSON",
+      ans3: "Creates a new object",
+      ans4: "Formats a string",
+      anskey: "Converts JSON to a string",
+    },
+  ];
+  
+
+
+var question = document.getElementById('question')
+var opt1 = document.getElementById('textOpt1')
+var opt2 = document.getElementById('textOpt2')
+var opt3 = document.getElementById('textOpt3')
+var opt4 = document.getElementById('textOpt4')
+var ans = document.getElementsByClassName('answer1')
+var btn = document.getElementById('next')
+var inp1 = document.getElementById('ans1')
+var inp2 = document.getElementById('ans2')
+var inp3 = document.getElementById('ans3')
+var inp4 = document.getElementById('ans4')
+
+opt1.style.cursor = 'pointer'
+opt2.style.cursor = 'pointer'
+opt3.style.cursor = 'pointer'
+opt4.style.cursor = 'pointer'
+
+var queCount = 0
+var score = 0
+
+function htmlcss() {
+    var getQuestion = htmlCssQuiz[queCount]
+    question.innerHTML = getQuestion.que
+    opt1.innerHTML = getQuestion.ans1
+    opt2.innerHTML = getQuestion.ans2
+    opt3.innerHTML = getQuestion.ans3
+    opt4.innerHTML = getQuestion.ans4
+    inp1.value = getQuestion.ans1
+    inp2.value = getQuestion.ans2
+    inp3.value = getQuestion.ans3
+    inp4.value = getQuestion.ans4
+}
+window.onload = htmlcss()
+
+function next() {
+    //Reading and checking value of selected option
+    for (var i = 0; i < ans.length; i++) {
+        if (ans[i].checked) {
+            console.log(ans[i].value)
+            if (ans[i].value === htmlCssQuiz[queCount].anskey) {
+                score++
+            }
+        }
+    }
+    console.log(score)
+    queCount++
+    //Next Question
+    if (queCount < htmlCssQuiz.length) {
+        htmlcss()
+        unSelect()
+
+    } else {
+        //Showing Result with Score
+        document.getElementById('quizWindow').style.display = 'none'
+        document.getElementById('resultWindow').style.display = 'block'
+        var totalQue = document.getElementById('totalQue')
+        var correctQue = document.getElementById('correctQue')
+        totalQue.innerText = htmlCssQuiz.length
+        correctQue.innerText = score
+        var per = document.getElementById('per')
+        var CalculatePer = (score / htmlCssQuiz.length) * 100
+        per.innerText = Math.round(CalculatePer) + "%"
+
+
+        //Conditions of Result display as per score
+        if (CalculatePer < 70) {
+            var msg = document.getElementById('result')
+            var dial = document.getElementById('dial')
+            msg.innerText = 'Better Luck Next Time ! You Failed'
+            msg.style.color = 'red'
+            dial.style.borderColor = 'red'
+        } else {
+            var msg = document.getElementById('result')
+            var dial = document.getElementById('dial')
+            msg.innerText = 'Congratulations ! You Passed'
+            msg.style.color = 'green'
+            dial.style.borderColor = 'green'
+        }
+    }
+}
+//Deselect next radio
+function unSelect() {
+    for (var i = 0; i < ans.length; i++) {
+        ans[i].checked = false
+    }
+}
+
+//return to home from result
+function back() {
+    window.location.href = '../welcome.html'
+}
+
+
+//**************** */ JavaScript Quiz ************************
+
+var questionjs = document.getElementById('questionjs')
+var opt1js = document.getElementById('textOpt1js')
+var opt2js = document.getElementById('textOpt2js')
+var opt3js = document.getElementById('textOpt3js')
+var opt4js = document.getElementById('textOpt4js')
+var ansjs = document.getElementsByClassName('answer1js')
+var btnjs = document.getElementById('nextjs')
+var inp1js = document.getElementById('ans1js')
+var inp2js = document.getElementById('ans2js')
+var inp3js = document.getElementById('ans3js')
+var inp4js = document.getElementById('ans4js')
+
+opt1js.style.cursor = 'pointer'
+opt2js.style.cursor = 'pointer'
+opt3js.style.cursor = 'pointer'
+opt4js.style.cursor = 'pointer'
+
+var queCountjs = 0
+var scorejs = 0
+
+function javascript() {
+    var getQuestionjs = jsQuiz[queCountjs]
+    questionjs.innerHTML = getQuestionjs.que
+    opt1js.innerHTML = getQuestionjs.ans1
+    opt2js.innerHTML = getQuestionjs.ans2
+    opt3js.innerHTML = getQuestionjs.ans3
+    opt4js.innerHTML = getQuestionjs.ans4
+    inp1js.value = getQuestionjs.ans1
+    inp2js.value = getQuestionjs.ans2
+    inp3js.value = getQuestionjs.ans3
+    inp4js.value = getQuestionjs.ans4
+}
+window.onload = javascript()
+
+function nextjs() {
+    //Reading and checking value of selected option
+    for (var i = 0; i < ansjs.length; i++) {
+        if (ansjs[i].checked) {
+            console.log(ansjs[i].value)
+            if (ansjs[i].value === jsQuiz[queCountjs].anskey) {
+                scorejs++
+            }
+        }
+    }
+    console.log(scorejs)
+    queCountjs++
+    //Next Question
+    if (queCountjs < jsQuiz.length) {
+        javascript()
+        unSelect()
+
+    } else {
+        //Showing Result with Score
+        document.getElementById('quizWindow').style.display = 'none'
+        document.getElementById('resultWindow').style.display = 'block'
+        var totalQuejs = document.getElementById('totalQuejs')
+        var correctQuejs = document.getElementById('correctQuejs')
+        totalQuejs.innerText = jsQuiz.length
+        correctQuejs.innerText = score
+        var perjs = document.getElementById('perjs')
+        var CalculatePerjs = (scorejs / jsQuiz.length) * 100
+        perjs.innerText = Math.round(CalculatePerjs) + "%"
+
+
+        //Conditions of Result display as per score
+        if (CalculatePerjs < 70) {
+            var msg = document.getElementById('result')
+            var dial = document.getElementById('dial')
+            msg.innerText = 'Better Luck Next Time ! You Failed'
+            msg.style.color = 'red'
+            dial.style.borderColor = 'red'
+        } else {
+            var msg = document.getElementById('result')
+            var dial = document.getElementById('dial')
+            msg.innerText = 'Congratulations ! You Passed'
+            msg.style.color = 'green'
+            dial.style.borderColor = 'green'
+        }
+    }
+}
+//Deselect next radio
+function unSelect() {
+    for (var i = 0; i < ans.length; i++) {
+        ans[i].checked = false
+    }
+}
+
+//return to home from result
+function back() {
+    window.location.href = '../welcome.html'
+}
